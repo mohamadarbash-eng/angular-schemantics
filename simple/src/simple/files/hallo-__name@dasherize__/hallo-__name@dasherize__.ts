1cd50e05745b2ee021930ab81b5b1edc0a74cd4c
Arbash Mohamad <eng@mohamadarbash.com>
2020-02-22 23:05:56 +0100

    integrete schematic with angular workspace

---
+++ simple/src/simple/files/hallo-__name@dasherize__/hallo-__name@dasherize__.ts
@@ -0,0 +1,1 @@
+console.log(`'Hello <%= addPrefix(name) %>`);
--- simple/src/simple/index.ts
+++ simple/src/simple/index.ts
@@ -1,15 +1,50 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, move, Rule, SchematicContext, SchematicsException, template, Tree, url } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { parsename } from '@schematics/angular/utility/parse-name';
 
 export interface Schema {
   name: string;
+  project?: string;
 }
 // You don't have to export the function as default. You can also have more than one rule factory
-// per file.
+// per files.
 export function simple(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
-        const {name} = _options;
-        tree.delete('hello.js');
-        tree.create('hello.js', `console.log('Hallo ${name}');`);
-        return tree;
+        const workspaceConfigBuffer = tree.read("angular.json");
+        if (!workspaceConfigBuffer) {
+            throw new SchematicsException("No an Angular CLI Workspace");
+        }
+        const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+        const projectName = _options.project || workspaceConfig.defaultProject;
+        const project = workspaceConfig.projects[projectName];
+
+        const defaulProjectPath = buildDefaultPath(project);
+
+        const parsepath = parsename(defaulProjectPath, _options.name);
+
+        const { name, path } = parsepath;
+
+        const sourceTemplates = url('./files');
+        const sourceParamterizedTemplate = apply(sourceTemplates, [
+
+            template({
+                ..._options,
+                ...strings,
+                name
+            }),
+            move(path)
+        ]);
+function addPrefix(value: string): string {
+    return 'Mr/s' + value;
+}
+
+
+
+      //  const {name} = _options;
+      //  tree.delete('hello.js');
+      //  tree.create('hello.js', `console.log('Hallo ${name}');`);
+
+        return mergeWith(sourceParamterizedTemplate)(tree, _context);
     };
 }
