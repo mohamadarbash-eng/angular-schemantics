86355fe351374dc71aa8124d9e9c667fa57c4d15
Arbash Mohamad <eng@mohamadarbash.com>
2020-02-22 21:36:58 +0100

    add logic to create hallo file

--- simple/src/simple/index.ts
+++ simple/src/simple/index.ts
@@ -1,10 +1,15 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
+export interface Schema {
+  name: string;
+}
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function simple(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+export function simple(_options: Schema): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+        const {name} = _options;
+        tree.delete('hello.js');
+        tree.create('hello.js', `console.log('Hallo ${name}');`);
+        return tree;
+    };
 }
